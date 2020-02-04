869ecb63d6af94940329d8a547ae721e01ea1e56
Retr0_ <dennylungu19@gmail.com>
2020-02-04 20:33:47 +0100

    v2

--- src/main/java/com/lungovski19/newdoors/NewDoorsMod.java
+++ src/main/java/com/lungovski19/newdoors/NewDoorsMod.java
@@ -1,6 +1,6 @@
 package com.lungovski19.newdoors;
 
-import com.lungovski19.newdoors.blocks.StoneDoor;
+import com.lungovski19.newdoors.blocks.NewDoorBlock;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
@@ -10,11 +10,13 @@ import net.minecraft.block.DoorBlock;
 public class NewDoorsMod implements ModInitializer {
 	public static final String MOD_ID = "newdoors";
 
-	public static StoneDoor stone_doorblock;
+	public static NewDoorBlock stone_doorblock;
+	public static NewDoorBlock sandstone_doorblock;
 
 	@Override
 	public void onInitialize() {
-		stone_doorblock = new StoneDoor(Block.Settings.copy(Blocks.OAK_DOOR), "stone_door");
+		stone_doorblock = new NewDoorBlock(Block.Settings.copy(Blocks.OAK_DOOR), "stone_door");
+		sandstone_doorblock = new NewDoorBlock(Block.Settings.copy(Blocks.OAK_DOOR), "sandstone_door");
 	}
 
 	public static BlockState copyState(BlockState copyState) {
--- src/main/java/com/lungovski19/newdoors/mixin/MixinDoorBlock.java
+++ src/main/java/com/lungovski19/newdoors/mixin/MixinDoorBlock.java
@@ -1,12 +1,11 @@
 package com.lungovski19.newdoors.mixin;
 
 import com.lungovski19.newdoors.NewDoorsMod;
-import com.lungovski19.newdoors.blocks.StoneDoor;
+import com.lungovski19.newdoors.blocks.NewDoorBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.DoorBlock;
 import net.minecraft.block.enums.DoubleBlockHalf;
-import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -18,7 +17,6 @@ import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(DoorBlock.class)
@@ -27,7 +25,7 @@ public abstract class MixinDoorBlock {
 	@Inject(method = "onUse", at = @At("HEAD"), cancellable = true)
 	private void newdoors$activate(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> callbackInfoReturnable) {
 		ItemStack heldStack = player.inventory.getMainHandStack();
-		if (!(state.getBlock() instanceof StoneDoor) && heldStack.getItem() == Items.GLASS_PANE) {
+		if (!(state.getBlock() instanceof NewDoorBlock) && heldStack.getItem() == Items.GLASS_PANE) {
 			if (state.get(DoorBlock.HALF) == DoubleBlockHalf.UPPER) pos = pos.down();
 
 			BlockState newdoorsState = NewDoorsMod.copyState(state);
